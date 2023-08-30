dda0bfd595f16a33898aeedb244574dfc8b1b8b8
Tomas Alvarez <alvareztomasignacio@gmail.com>
2023-08-30 00:01:59 -0300

    Authentication logic added

--- PageChino/PageChinoFE/urls.py
+++ PageChino/PageChinoFE/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.HomeVista.as_view(), name="home"),
+    path('login',views.LoginVista.as_view(), name="login"),
+    path('logout',views.logoutUser, name="logout"),
+    
+    # Template Authentication Views
+    # path("accounts/", include("django.contrib.auth.urls")),
 ]
--- PageChino/PageChinoFE/views.py
+++ PageChino/PageChinoFE/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login, logout
 
 class HomeVista(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
+
         return context
+
+class LoginVista(TemplateView):
+    template_name = 'login.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            print("Usuario Correcto")
+            login(request,user)
+        else:
+            print("Usuario Incorrecto")
+                
+        return redirect('home')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
